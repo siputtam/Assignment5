14a1ee5065b674031cb6a4ebf253dee8a5240064
Uttam Kumar Das <59952873+siputtam@users.noreply.github.com>
2020-01-29 11:21:34 +0530

    Add files via upload

---
+++ assignment5/assignment5/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Creation Of dictionary
+            Dictionary<int, string> d = new Dictionary<int, string>();
+            d.Add(1998, "Atal Bihari Vajpayee");
+            d.Add(2014, "Narendra Modi");
+            d.Add(2004, "Manmohan Singh");
+            Console.WriteLine("Prime minister of 2004 : "+d[2004]);
+            // Add Current Prime Minister
+            d.Add(2020, "Narendra Modi");
+            Console.WriteLine("\nCurrent Prime Minister : " + d[2020]);
+            //Sorting dictionary
+            d.OrderBy(x => x.Value);
+            Console.WriteLine("\nAfter Sorting list of Prime Ministers :");
+            foreach( var dict in d)
+            {
+                Console.WriteLine(dict.Key+" : " +dict.Value);
+            }
+
+        }
+    }
+}
